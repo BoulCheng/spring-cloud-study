2e2dff01c599404316bd51d9353b15fb61bd098f
BoulCheng <1241292849@qq.com>
2018-07-19 20:26:11 +0800

    ribbon-eureka-server

---
+++ spring-cloud-ribbon/ribbon-eureka-server/src/main/java/com/zlb/ribboneurekaserver/RibbonEurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.zlb.ribboneurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RibbonEurekaServerApplication {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(RibbonEurekaServerApplication.class, args);
+        new SpringApplicationBuilder(RibbonEurekaServerApplication.class).web(true).run(args);
+
+    }
+}
